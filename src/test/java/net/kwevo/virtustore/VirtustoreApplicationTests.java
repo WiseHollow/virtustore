81785a0dc03fa6b0500f48d498833ed57d6876e9
John Brooks <WiseHollow@users.noreply.github.com>
2021-08-21 19:26:03 -0500

    Added base springboot files, and gradle.

---
+++ src/test/java/net/kwevo/virtustore/VirtustoreApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kwevo.virtustore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtustoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
